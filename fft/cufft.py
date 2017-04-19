3246834fd4330a72e035f10c60b0c3e2207f2638
honestest <pengcao@Pengs-MacBook-Pro.local>
2017-04-18 20:37:42 -0700

    normalized ifftnc2c_cuda by 1/N

--- fft/cufft.py
+++ fft/cufft.py
@@ -8,7 +8,7 @@ def fftnc2c_cuda( x ):
     x_gpu  = gpuarray.to_gpu(x)
     xf_gpu = gpuarray.empty(x.shape, np.complex64)
     plan   = Plan(x.shape, np.complex64, np.complex64)
-    fft(x_gpu, xf_gpu, plan)  
+    fft(x_gpu, xf_gpu, plan)
     return xf_gpu.get()
 
 
@@ -17,6 +17,5 @@ def ifftnc2c_cuda( x ):
     x_gpu  = gpuarray.to_gpu(x)
     xf_gpu = gpuarray.empty(x.shape, np.complex64)
     plan   = Plan(x.shape, np.complex64, np.complex64)
-    ifft(x_gpu, xf_gpu, plan)  
-    return xf_gpu.get()
-     
+    ifft(x_gpu, xf_gpu, plan)
+    return xf_gpu.get()/np.prod(x.shape)
